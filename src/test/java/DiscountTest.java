fede0683ff2c7c9171a469895020519c4417758e
orangelily <547207352@qq.com>
2016-07-10 21:45:47 +0800

    商品价格不存在优惠信息打印小票测试

--- src/test/java/DiscountTest.java
+++ src/test/java/DiscountTest.java
@@ -17,14 +17,14 @@ public class DiscountTest {
 
     //
     @Test
-    public void discount_product_when_contains_discount_with_price_lower_100(){
+    public void print_product_when_contains_discount_with_price_lower_100(){
         Product product =new Product("Item001","basketball","个","球类","篮球",79.0);
         Double price = product.getPrice();
         System.out.println("名称:"+product.getName()+" 数量:1,单价:"+product.getPrice()+",小计:"+product.getPrice());
     }
 
     @Test
-    public void discount_product_when_contains_discount_with_price_higher_100(){
+    public void print_product_when_contains_discount_with_price_higher_100(){
         Product product =new Product("Item001","basketball","个","球类","篮球",189.0);
         Double price = product.getPrice();
         if (price.compareTo(100.0)>0){
@@ -33,7 +33,13 @@ public class DiscountTest {
         System.out.println("名称:"+product.getName()+" 数量:1,单价:"+product.getPrice()+",小计:"+product.getPrice());
     }
 
+    @Test
+    public void print_product_when_no_discount_with_price(){
+        Product product =new Product("Item001","basketball","个","球类","篮球",289.0);
+        Double price = product.getPrice();
 
+        System.out.println("名称:"+product.getName()+" 数量:1,单价:"+product.getPrice()+",小计:"+product.getPrice());
+    }
 
     //
     @Test
