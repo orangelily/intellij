167368cfa98d943e5f8fece11a8c958958a65747
orangelily <547207352@qq.com>
2016-07-10 18:10:48 +0800

    商店中的商品信息增删改操作测试

---
+++ src/test/java/ProductTest.java
@@ -0,0 +1,65 @@
+/**
+ * Created by orange on 16/7/3.
+ */
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+public class ProductTest {
+    //商店中商品信息
+    private Map<String,Product> mapInShop = new HashMap<>();
+
+
+    @Test
+    public void add_one_product_with_info(){
+        Product product =new Product("Item001","basketball","个","球类","篮球",89.0);
+        Product productInShop = mapInShop.get("Item001");
+
+        if (productInShop!=null){
+            //商店中存在该商品信息,不能重复添加该商品,
+            // 后续可修改该方法记录商店中的对应商品数目mapInshop<String,Integer> (<条形码,数量>)
+            Boolean product_null = true;
+            assertTrue(product_null);
+        }else {
+            mapInShop.put(product.getBarcode(),product);
+        }
+
+    }
+
+    @Test
+    public void modify_one_product_with_info(){
+        Product product =new Product("Item001","basketball","个","球类","篮球",89.0);
+        Product productInShop = mapInShop.get("Item001");
+
+        if (productInShop!=null){
+//            assertTrue(productInShop!=null);
+            mapInShop.put(product.getBarcode(),product);
+        }else {
+            //不存在该商品信息,无法修改,只能另行做添加处理
+            Boolean product_in_shop_null = true;
+            assertTrue(product_in_shop_null);
+        }
+
+    }
+
+    @Test
+    public void remove_one_product_with_info(){
+        Product product =new Product("Item001","basketball","个","球类","篮球",89.0);
+        Product productInShop = mapInShop.get("Item001");
+
+        if (productInShop!=null){
+//            assertTrue(productInShop!=null);
+            mapInShop.remove(product.getBarcode());
+        }else {
+            //不存在该商品信息,无法删除
+            Boolean product_in_shop_null = true;
+            assertTrue(product_in_shop_null);
+        }
+
+    }
+
+
+}
